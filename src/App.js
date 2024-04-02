969b3b42e8546e5c07c2a2da15d5b568f65fef9d
Shobindiran <shobindiran@gmail.com>
2024-04-02 21:36:02 +0530

    fixed props drilling using Component Composition

--- src/App.js
+++ src/App.js
@@ -56,19 +56,27 @@ const average = (arr) =>
 
     return (
       <>
-        <NavBar movies={movies}/>
-        <Main movies={movies}/>
+        <NavBar>
+          <Search/>
+          <NumResults movies={movies}/>
+        </NavBar>
+         
+        <Main>
+          <ListBox>
+            <MovieList movies={movies} />
+          </ListBox>
+          <WatchedBox/>
+        </Main>
       </>
     );
   }
 
-function NavBar({movies}){
+function NavBar({children}){
   
   return(
     <nav className="nav-bar">
         <Logo/>
-        <Search/>
-        <NumResults movies={movies}/>
+        {children}
     </nav>
   )
 }
@@ -105,18 +113,17 @@ function NumResults({movies}){
 }
 
 
-function Main({movies}){
+function Main({children}){
   
   return(
     <main className="main">
-        <ListBox movies={movies}/>
-        <WatchedBox/>
+        {children}
     </main>
 
 )
 }
 
-function ListBox({movies}){
+function ListBox({children}){
   const [isOpen1, setIsOpen1] = useState(true);
 
   return(
@@ -127,9 +134,7 @@ function ListBox({movies}){
           >
             {isOpen1 ? "–" : "+"}
           </button>
-          {isOpen1 && (
-            <MovieList movies={movies} />
-          )}
+          {isOpen1 && children}
         </div>
   )
 }
