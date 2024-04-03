64acb04162346e1dbdb29720f229dc5bd98f45ac
Shobindiran <shobindiran@gmail.com>
2024-04-03 20:56:31 +0530

    Created a Reusable Star rating Component

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ const average = (arr) =>
 
   export default function App() {
     const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
 
     return (
       <>
@@ -62,10 +63,14 @@ const average = (arr) =>
         </NavBar>
          
         <Main>
-          <ListBox>
+          <Box>
             <MovieList movies={movies} />
-          </ListBox>
-          <WatchedBox/>
+          </Box>
+
+          <Box>
+            <WatchedSummary watched={watched}/>
+            <WatchedMovieList watched={watched}/>
+          </Box>
         </Main>
       </>
     );
@@ -123,21 +128,22 @@ function Main({children}){
 )
 }
 
-function ListBox({children}){
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({children}){
+  const [isOpen, setIsOpen] = useState(true);
 
   return(
     <div className="box">
           <button
             className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}
+            onClick={() => setIsOpen((open) => !open)}
           >
-            {isOpen1 ? "–" : "+"}
+            {isOpen ? "–" : "+"}
           </button>
-          {isOpen1 && children}
+          {isOpen && children}
         </div>
   )
 }
+
 
 function MovieList({movies}){
 
@@ -163,28 +169,6 @@ function Movie({movie}){
         </p>
       </div>
     </li>
-  )
-}
-
-function WatchedBox(){
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-  
-  return(
-    <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen2((open) => !open)}
-            >
-            {isOpen2 ? "–" : "+"}
-          </button>
-          {isOpen2 && (
-            <>
-              <WatchedSummary watched={watched}/>
-              <WatchedMovieList watched={watched}/>
-            </>
-          )}
-        </div>
   )
 }
 
