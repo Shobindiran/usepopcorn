3fcb08c13b8b154af14aa7a825316f8fbff5e240
Shobindiran <shobindiran@gmail.com>
2024-04-16 19:55:21 +0530

    search movie connected with api useffects

--- src/App.js
+++ src/App.js
@@ -53,15 +53,16 @@ const average = (arr) =>
   const KEY = "f84fc31d";
 
   export default function App() {
+    const [query, setQuery] = useState("");
     const [movies, setMovies] = useState([]);
     const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error,setError] = useState("");
-    const query = "interstellar";
     useEffect(()=>{
       async function fetchMovies(){
         try{
           setIsLoading(true);
+          setError("");
           const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
           
           if(!res.ok) throw new Error("Something went wrong in fectching movies")
@@ -82,13 +83,19 @@ const average = (arr) =>
         }
       }
 
+      if(!query.length){
+        setMovies([]);
+        setError("");
+        return
+      } 
+
       fetchMovies();
-    },[]);
+    },[query]);
 
     return (
       <>
         <NavBar>
-          <Search/>
+          <Search query={query} setQuery={setQuery}/>
            <NumResults movies={movies}/>
         </NavBar>
           
@@ -136,9 +143,7 @@ function Logo(){
   )
 }
 
-function Search(){
-  const [query, setQuery] = useState("");
-
+function Search({query, setQuery}){
   return(
     <input
           className="search"
